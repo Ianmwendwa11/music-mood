1efd6ebd287af211a8ac42e32ac15112b1e1271c
ianmwendwa11 <ian.musyoki12@gmail.com>
2025-03-25 16:53:11 +0300

    adds js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function changeBackground(mood) {
+    const body = document.body;
+    
+    
+    const moodBackgrounds = {
+        happy: "url('images/happy.jpg')",
+        sad: "url('images/sad.jpg')",
+        chill: "url('images/chill.jpg')",
+        romantic: "url('images/romantic.jpg')",
+        hype: "url('images/hype.jpg')",
+        healing: "url('images/healing.jpg')",
+        "in love": "url('images/inlove.jpg')"
+    };
+
+   
+    body.style.backgroundImage = moodBackgrounds[mood] || "none";
+    body.style.transition = "background 0.5s ease-in-out";
+}
 
