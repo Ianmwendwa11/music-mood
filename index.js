5478cf672c2ba49d8a8d33264cbf8a88d5b51b14
ianmwendwa11 <ian.musyoki12@gmail.com>
2025-03-27 09:59:12 +0300

    correcting html and js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 function changeBackground(mood) {
     const body = document.body;
-    
-    
+
     const moodBackgrounds = {
         happy: "url('images/happy.jpeg')",
         sad: "url('images/sad.jpg')",
         chill: "url('images/chill.jpeg')",
         romantic: "url('images/romantic.jpeg')",
-        hype: "url('images/hypiano.jpg')",
+        hype: "url('images/hypiano.jpg')", 
         healing: "url('images/healing.jpeg')",
-        "in love": "url('images/inlove.jpg')"
+        inlove: "url('images/inlove.jpg')"
     };
 
-   
     body.style.backgroundImage = moodBackgrounds[mood] || "none";
+    body.style.backgroundSize = "cover";
+    body.style.backgroundPosition = "center";
     body.style.transition = "background 0.5s ease-in-out";
 }
-
